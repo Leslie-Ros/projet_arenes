4d6deb372da8344911494abbbf1d745e708f438d
aurelie <aurelie@lampaurelie>
2016-10-28 10:43:07 +0200

    ajout de l'image, modif des fichiers css

--- webroot/js/index.js
+++
@@ -1,8 +0,0 @@
-function clignotement(){ 
-if (document.getElementById("inscription").style.display=="block") 
-document.getElementById("inscription").style.display="none"; 
-else 
-document.getElementById("inscription").style.display="block"; 
-} 
-// mise en place de l appel régulier de la fonction toutes les 0.5 secondes 
-setInterval("clignotement()", 1000); 
